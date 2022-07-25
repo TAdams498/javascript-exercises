fbe659c62db3b68feb70d0bc31da80b940672590
Thomas Adams <tpadams402@gmail.com>
2022-07-25 17:52:46 -0600

    complete task 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  temp = (fTemp - 32) * (5/9);
+  cTemp = Math.round(temp * 10) / 10;
+  return cTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  temp = (cTemp * (9/5)) + 32;
+  fTemp = Math.round(temp * 10) / 10;
+  return fTemp;
 };
 
 // Do not edit below this line
