902c1be3d6e8b28d80e08512c5732e7e805727c0
Thomas Adams <tpadams402@gmail.com>
2022-07-25 17:09:46 -0600

    complete task 03;

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let strArray = [];
+  let reverseString = "";
+  for (let i = 0; i < string.length; i++) {
+    strArray.push(string.substring(i, i + 1));
+  }
+  temp = strArray.length;
+  for (let i = 0; i < temp; i++) {
+    reverseString += strArray.pop();
+  }
+  return reverseString;
 };
 
 // Do not edit below this line
