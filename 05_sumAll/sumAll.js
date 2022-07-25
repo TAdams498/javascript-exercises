1d30b4e126727aa00745a70977940f2ccea74a0d
Thomas Adams <tpadams402@gmail.com>
2022-07-25 17:39:46 -0600

    complete task 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (num2 < num1) {
+    let temp1 = num1;
+    let temp2 = num2;
+    num2 = temp1;
+    num1 = temp2;
+  }
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  if ((typeof num1 != "number") || (typeof num2 != "number")) {
+    return "ERROR";
+  }
+  let total = 0;
+  for (let i = num1; i < num2+1; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
