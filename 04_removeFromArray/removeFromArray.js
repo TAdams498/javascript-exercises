4960bb214fb92e6e74597c26107a6eaab4c16886
Thomas Adams <tpadams402@gmail.com>
2022-07-25 17:27:48 -0600

    complete task 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...args) {
+  let tempArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    let item = inputArray[i];
+    let valid = true;
+    for (let c = 0; c < args.length; c++) {
+      if (item === args[c]) {
+        valid = false;
+      }
+    }
+    if (valid) {
+      tempArray.push(inputArray[i])
+    }
+  }
+  return tempArray;
 };
 
 // Do not edit below this line
