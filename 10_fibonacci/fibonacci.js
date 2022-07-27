ea7bce383ddfc60a6f0c6ae643e55f59c5d6fe0f
Thomas Adams <tpadams402@gmail.com>
2022-07-27 10:38:53 -0600

    complete task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  let fibArr = [1, 1];
+  if (index < 0) {
+    return "OOPS";
+  }
+  if (fibArr[index]) {
+    return fibArr[index - 1];
+  }
+  else {
+    for (let i = 2; i < index; i++) {
+      fibArr[i] = fibArr[i-1] + fibArr[i-2];
+    }
+    return fibArr[index - 1];
+  }
 };
 
 // Do not edit below this line
