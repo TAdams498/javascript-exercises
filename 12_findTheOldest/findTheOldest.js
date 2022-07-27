ffc813bde4a88996c82ca5c18e8cfbda4b799e49
Thomas Adams <tpadams402@gmail.com>
2022-07-27 10:56:46 -0600

    complete task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const sorted = people.sort(function(a, b) {
+    let aDeathYear;
+    let bDeathYear;
+    if (!a.yearOfDeath) {
+      aDeathYear = new Date().getFullYear();
+    }
+    else {
+      aDeathYear = a.yearOfDeath;
+    }
+    if (!b.yearOfDeath) {
+      bDeathYear = new Date().getFullYear();
+    }
+    else {
+      bDeathYear = b.yearOfDeath;
+    }
+    return (aDeathYear - a.yearOfBirth) < (bDeathYear - b.yearOfBirth) ? 1 : -1;
+  });
+  return sorted[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+/*const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  }
+*/
