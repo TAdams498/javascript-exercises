7fba9ed20d61dd17872b9ac5da3809eb28d88a13
Thomas Adams <tpadams402@gmail.com>
2022-07-27 10:09:29 -0600

    complete task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((prev, next) => prev + next, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((prev, next) => prev * next, 1);
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	let numbers = [];
+	for (let i = 0; i < exp; i++) {
+		numbers.push(num);
+	}
+	return numbers.reduce((prev, next) => prev * next, 1);
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+	let numbers = []
+	for (let i = 1; i < fact + 1; i++) {
+		numbers.push(i);
+	}
+	return numbers.reduce((prev, next) => prev * next, 1);
 };
 
 // Do not edit below this line
