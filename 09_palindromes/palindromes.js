33553b24c13b37dd4fd2f26cb6a0c8a1cb6e4224
Thomas Adams <tpadams402@gmail.com>
2022-07-27 10:26:48 -0600

    complete task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let notAllowed = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "~", "`", "[", "{", "]", "}", "\\", "|", ";", ":", "\"", "'", "<", ",", ">", ".", "/", "?"];
+  const stringArray = Array.from(string.toLowerCase());
+  const filteredArray = stringArray.filter(character => !notAllowed.includes(character));
 
+  //iterate through arrays, one in reverse and check if match
+  let newString1 = filteredArray.reduce((a, b) => a + b, "");
+  filteredArrayR = filteredArray.reverse();
+  let newString2 = filteredArrayR.reduce((a, b) => a + b, "");
+
+  if (newString1 === newString2) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 // Do not edit below this line
